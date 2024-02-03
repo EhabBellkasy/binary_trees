4aca48224514301aff15a6a32a8d6eaea3d26d69
EhabBellkasy <EhabBellkasy@gmail.com>
2024-02-03 01:40:47 +0000

    134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -8,11 +8,11 @@
  */
 int count_h_nodes(binary_tree_t *root)
 {
-if (!root)
-return (0);
+	if (!root)
+		return (0);
 
-return (1 + count_h_nodes(root->left) +
-    count_h_nodes(root->right));
+	return (1 + count_h_nodes(root->left) +
+		    count_h_nodes(root->right));
 }
 
 /**
@@ -24,20 +24,20 @@ return (1 + count_h_nodes(root->left) +
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-int i, nodes, *arr = NULL;
+	int i, nodes, *arr = NULL;
 
-*size = 0;
-if (!heap)
-return (NULL);
+	*size = 0;
+	if (!heap)
+		return (NULL);
 
-nodes = count_h_nodes(heap);
-arr = malloc(sizeof(*arr) * nodes);
-if (!arr)
-return (NULL);
+	nodes = count_h_nodes(heap);
+	arr = malloc(sizeof(*arr) * nodes);
+	if (!arr)
+		return (NULL);
 
-*size = nodes;
-for (i = 0; i < nodes; i++)
-arr[i] = heap_extract(&heap);
+	*size = nodes;
+	for (i = 0; i < nodes; i++)
+		arr[i] = heap_extract(&heap);
 
-return (arr);
+	return (arr);
 }
