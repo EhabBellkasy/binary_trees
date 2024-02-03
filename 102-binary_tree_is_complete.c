c73606d929a27f79fd7cfd0ddc6cd0426f243f6b
EhabBellkasy <EhabBellkasy@gmail.com>
2024-02-03 01:09:50 +0000

    0x1D. C - Binary trees

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
-
 /**
- * new_node - function that creates a new_node in a linked_list
- * @node: the type pointer of node to be created
+ * new_node - Function that creates a new_node in a linked_list
+ * @node: Type pointer of node to be created
  * Return: the node created
  */
 link_t *new_node(binary_tree_t *node)
@@ -20,8 +19,8 @@ new->next = NULL;
 return (new);
 }
 /**
- * free_q - function that free the nodes at the linked list
- * @head: node of the linked_list
+ * free_q - Function that free the nodes at the linked list
+ * @head: Node of the linked_list
  */
 void free_q(link_t *head)
 {
@@ -35,10 +34,10 @@ head = temp_node;
 }
 }
 /**
- * _push - function that pushes a node into the stack
- * @node: type pointer of node of the tree
- * @head: type head node of in the stack
- * @tail: type tail node of in the stack
+ * _push - Function that pushes a node into the stack
+ * @node: Type pointer of node of the tree
+ * @head: Type head node of in the stack
+ * @tail: Type tail node of in the stack
  */
 void _push(binary_tree_t *node, link_t *head, link_t **tail)
 {
@@ -54,8 +53,8 @@ exit(1);
 *tail = new;
 }
 /**
- * _pop - function that pops a node into the stack
- * @head: type head node of in the stack
+ * _pop - Function that pops a node into the stack
+ * @head: Type head node of in the stack
  */
 void _pop(link_t **head)
 {
@@ -66,8 +65,8 @@ free(*head);
 *head = temp_node;
 }
 /**
- * binary_tree_is_complete - function that checks if a binary tree is complete
- * @tree: the type pointer of node of the tree
+ * binary_tree_is_complete - Function that checks if a binary tree is complete
+ * @tree: Type pointer of node of the tree
  * Return: 1 if is complete 0 if it is not
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
